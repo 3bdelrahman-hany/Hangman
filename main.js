4a3239165214a87b9cbc2747d7e08f79eee354a7
3abdelrahman-hany <3abdohany7@gmail.com>
2026-03-11 22:35:29 +0200

    changing the api

--- main.js
+++ main.js
@@ -18,24 +18,140 @@ lettersArray.forEach(letter => {
 // 2. Fetch a random word from API
 // ============================
 
-fetch("https://random-word-api.vercel.app/api?words=1")
-  .then(res => res.json())
-  .then(data => {
-    let randomValueValue = data[0]; // Random word from API
-    startGame(randomValueValue);
-    console.log(data)
-  })
-  .catch(err => {
-    console.error("Error fetching random word:", err);
-    startGame("fallback"); // fallback word
-  });
+
+let random=['Animals','Sports','Programming Languages','Games',"Wordle","Countries","Capitals of Countries","Birds"];
+
+let randValue=random[Math.floor(Math.random()*8)];
+console.log(randValue);
+
+switch(randValue){
+  case 'Animals' :
+    async function getAnimals() {
+      try {
+        let response = (await fetch("https://random-words-api.kushcreates.com/api?language=en&category=animals"));
+        let theword = await response.json()
+        let value = theword[Math.floor(Math.random()*140)].word
+        startGame(value)
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    }
+
+    getAnimals();
+    break;
+
+  case 'Sports':
+    async function getSports() {
+      try {
+        let response = (await fetch("https://random-words-api.kushcreates.com/api?language=en&category=sports"));
+        let theword = await response.json()
+        let value = theword[Math.floor(Math.random()*78)].word
+        
+        startGame(value)
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    }
+
+    getSports();
+
+    break;
+  case "Games":
+    async function getGames() {
+      try {
+        let response= (await fetch("https://random-words-api.kushcreates.com/api?language=en&category=games"))
+        let theword=await response.json()
+        let value=theword[Math.floor(Math.random()*88)].word;
+        
+        startGame(value)
+      } catch (error) {
+          console.log("Error fetching data:", error);
+      }
+    }
+
+    getGames();
+    break;
+
+  case "Capitals of Countries":
+    async function getCapitalsOfCountries() {
+      try {
+        let response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=capitals_of_countries");
+        let theword= await response.json()
+        let value=theword[Math.floor(Math.random()*201)].word
+        startGame(value)
+      } catch (error) {
+          console.log("Error fetching data:", error);
+      }
+    }
+
+    getCapitalsOfCountries();
+    break;
+  case "Programming Languages":
+      async function getPL(){
+        try {
+          let response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=programming_languages")
+          let Words =  await response.json();
+          let value = Words[Math.floor(Math.random()*57)].word;
+          startGame(value);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+      }
+      getPL();
+      break;
+
+  case "Countries":
+    async function getCountry(){
+        try {
+          let response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=countries");
+          let Words =  await response.json();
+          let value = Words[Math.floor(Math.random()*200)].word;
+          startGame(value);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+      }
+      getCountry();
+      break;
+  
+  case"Birds":
+      async function getBird(){
+        try {
+          let response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=birds");
+          let Words =  await response.json();
+          let value = Words[Math.floor(Math.random()*60)].word;
+          startGame(value);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+      }
+      getBird();
+      break;
+
+  case"Wordle":
+      async function getWordle(){
+        try {
+          let response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=wordle");
+          let Words =  await response.json();
+          let value = Words[Math.floor(Math.random()*14855)].word;
+          startGame(value);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+      }
+      getWordle();
+      break;
+
+}
+
+
 
 // ============================
 // 3. Main Game Function
 // ============================
 function startGame(randomValueValue) {
   // Set Category Info
-  document.querySelector(".game-info .category span").innerHTML = "Random";
+  document.querySelector(".game-info .category span").innerHTML = randValue;
 
   // Select Letter Guess Element
   let letterGuessContainer = document.querySelector(".theletter-guess");
